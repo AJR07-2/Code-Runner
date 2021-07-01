41f7166f42cd00790145743c736ddcf6c76ae3fe
Ang Jun Ray <angjunray@live.com>
2021-07-01 22:36:03 +0800

    Update countdown.js

--- src/commands/countdown.js
+++ src/commands/countdown.js
@@ -5,18 +5,19 @@ module.exports = async (message) => {
   content = content.substring(1);
   let arr = content.split(" "),
     time = 0,
-		countdownMessage = "";
-	
+    countdownMessage = "";
+
   if (arr.length >= 5) {
-    time += parseInt(arr[1]) * 60 * 60 + parseInt(arr[2]) * 60 + parseInt(arr[3]);
-		for (let i = 4; i < arr.length; i++) countdownMessage += arr[i] + " ";
-		if (isNaN(time)) {
-			invalidSyntax(
-				message,
-				"Please provide date first then time, check help for more info"
-			);
-			return;
-		}
+    time +=
+      parseInt(arr[1]) * 60 * 60 + parseInt(arr[2]) * 60 + parseInt(arr[3]);
+    for (let i = 4; i < arr.length; i++) countdownMessage += arr[i] + " ";
+    if (isNaN(time)) {
+      invalidSyntax(
+        message,
+        "Please provide date first then time, check help for more info"
+      );
+      return;
+    }
 
     let sent = await message.channel.send(
       new Discord.MessageEmbed()
@@ -25,10 +26,9 @@ module.exports = async (message) => {
         .setFooter("Code Runner!")
         .addField("Initalizing...", "Waiting...", true)
         .addField("Message", countdownMessage, true)
-		);
+    );
 
-		startCountdown(time, sent, countdownMessage);
-
+    startCountdown(time, sent, countdownMessage, message);
   } else {
     invalidSyntax(
       message,
@@ -42,24 +42,29 @@ function parseSeconds(time) {
   time -= seconds;
   let minutes = (time % 3660) / 60;
   time -= minutes * 60;
-	let hours = time / 3600;
+  let hours = time / 3660;
   return [seconds, minutes, hours];
 }
 
-function startCountdown(time, message, countdownMessage) {
-  let [seconds, minutes, hours] = parseSeconds(time), colour = "0000ff";
+function startCountdown(time, message, countdownMessage, authorMessage) {
+  let [seconds, minutes, hours] = parseSeconds(time),
+    colour = "0000ff";
   time--;
   if (time === 0) {
+    message.channel.send(
+      "<@" + authorMessage.author.id + "> " + `Countdown "${countdownMessage}" is over!`
+    );
     message.edit(
       new Discord.MessageEmbed()
-        .setColor("00ff00")
-        .setTitle(`COUNTDOWN WITH NAME OF "${countdownMessage}" HAS COMPLETED!!!`)
-    );
-		return;
+      .setColor(colour)
+      .setTitle(`Countdown Over`)
+      .setFooter("Code Runner!")
+    )
+    return;
   }
 
   if (time < 5) colour = "ff0000";
-  
+
   message.edit(
     new Discord.MessageEmbed()
       .setColor(colour)
@@ -69,6 +74,6 @@ function startCountdown(time, message, countdownMessage) {
       .addField("Message", countdownMessage, true)
   );
   setTimeout(() => {
-    startCountdown(time, message, countdownMessage);
+    startCountdown(time, message, countdownMessage, authorMessage);
   }, 1000);
 }
